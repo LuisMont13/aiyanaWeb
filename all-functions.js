f8335a4473f4bf73e28b3bbdcb3f80eb675ec50e
LuisMont13 <90803215+LuisMont13@users.noreply.github.com>
2023-07-25 17:42:20 -0700

    Delete all-functions.js

--- all-functions.js
+++
@@ -1,9 +0,0 @@
-function showMenu(){
-    var navLinks = document.getElementById("navLinks");
-    navLinks.style.right= "0";
-}
-
-function hideMenu(){
-    var navLinks = document.getElementById("navLinks");
-    navLinks.style.right= "-200px";
-}
